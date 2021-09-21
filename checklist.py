965f46ada4bef44f8c1458bf34e43324cd832f33
Jennifer Carreno <jennifercarreno72@gmail.com>
2021-09-21 16:17:40 -0700

    final touches to checklist

--- checklist.py
+++ checklist.py
@@ -28,17 +28,29 @@ def list_all_items():
 
 #mark completed
 def mark_completed(index):
-    checklist[index].append("√")
+    checklist[index] = "√ {}".format(checklist[index])
+    print(checklist[index])
+    return checklist[index]
 
 def select(function_code):
+    function_code = function_code.upper()
     #create item
-    if function_code == "C":
+    if function_code == "A":
         input_item = user_input("Input Item: ")
         create(input_item)
     #read item
     elif function_code == "R":
         item_index = user_input("Index Number? -> ")
         read(item_index)
+    #Update item
+    elif function_code == "U":
+        index = user_input("Index Number: ")
+        item = user_input("Input Item: ")
+        update(int(index), item)
+    #mark as complete 
+    elif function_code == "C":
+        index = user_input("Index Number: ")
+        mark_completed(int(index))
     #print all items
     elif function_code == "P":
         list_all_items()
@@ -54,14 +66,8 @@ def user_input(prompt):
     user_input = input(prompt)
     return user_input
 
-#test function
-# def test():
-#     print(user_value)
-   
-# test()
-
 running = True
 while running:
     selection = user_input(
-        "Press C to add to list, R to Read from list and P to display list and Q to quit -> ")
+        "Press A to add to list, R to Read from list, U to update list, C to mark as completed, and P to display list and Q to quit -> ")
     running = select(selection)
